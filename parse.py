ec0a4b8a0b1199abeb3ed3650a4b0d6ee4a716d8
Jman005 <n/a>
2018-12-26 20:35:53 -0500

    Fixed template formatting

--- parse.py
+++ parse.py
@@ -1,30 +1,27 @@
 ### imports ###
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import requests
+import numpy
 import json
+import time
 from reddit_parse import mlplt_bargraph as bgraph
 ###############
 
 
 ### functions/libs ###
 
-def record(text: str):
-    with open("logs\\datalog.txt", "a") as f:
+def log(text: str, fname: str):
+    with open(fname, "a") as f:
         f.write("\n" + text)
 
 
-def log(text: str):
-    with open("logs\\log.txt", "a") as f:
-        f.write("\n" + text)
-
-
-def pushshift_get(num, sub, time):
+def pushshift_get(sub, stime, etime):
     # slightly based on https://gist.github.com/dylankilkenny/3dbf6123527260165f8c5c3bc3ee331b, so thanks
     response = requests.get("https://api.pushshift.io/reddit/search/submission?"
-                            "&size=%s"
+                            "&before=%s"
                             "&after=%s"
                             "&subreddit=%s"
-                            % (num, time, sub))
+                            % (etime, stime, sub))
     return json.loads(response.text)['data']
 
 def get_providers():
@@ -42,10 +39,10 @@ def img_from_link(url):
             return Image.open(data)
 
         except (OSError, AttributeError) as e:
-            log("Could not load image of post %s. Exception: %s" % (url, e))
+            log("Could not load image of post %s. Exception: %s" % (url, e),"logs\\log.txt")
 
         except requests.exceptions.ConnectionError as e:
-            log("Timeout when trying to parse image of post %s. Exception: %s" % (url, e))
+            log("Timeout when trying to parse image of post %s. Exception: %s" % (url, e),"logs\\log.txt")
 
     return None  # We've either hit an error or a non-compliant post, so skip this one
 
@@ -64,19 +61,71 @@ def post_worker(post, ibase: list, name):
                     the box to paste the extracted image
                     as the second argument
     Argument name: The name of the image to be saved."""
+    def get_text_centered(text, font, size, pos):
+        fnt = ImageFont.truetype(font, size, encoding='unic')
+        size = fnt.getsize(text)
+        return pos - size[0]/2
 
     base, pos = ibase[0], ibase[1]
+    base_new = base.copy()
     img = img_from_link(post['url'])
     if img is not None:
-        if img.size[0] > 1214 or img.size[1] > 638:
-            img = img.resize((1213, 637))
-        blank = Image.new("RGBA", (1213, 637)) # why does Pillow force us to create another Image for pasting????
+        target_x = pos[2] - pos[0]
+        target_y = pos[3] - pos[1]
+        if img.size[0] > target_x or img.size[1] > target_y:
+            img = img.resize((target_x, target_y))
+        blank = Image.new("RGB", (target_x, target_y)) # why does Pillow force us to create another Image for pasting????
         blank.paste(img,
                     (int(round((blank.size[0]-img.size[0])/2)),
                      int(round((blank.size[1]-img.size[1])/2))))
-        base.paste(blank, pos)
-        base.save("out\\%s" % name)
+        base_new.paste(blank, pos)
+
+        # Drawing the text.....
+        draw = ImageDraw.Draw(base_new)
+        titlepos = get_text_centered(post['title'], "reddit_parse\\resources\\symbola.ttf",108,2076)
+        authpos = get_text_centered("by /u" + post['author'], "reddit_parse\\resources\\symbola.ttf", 48, 2060)
+        scorepos = get_text_centered("karma:" + str(post['score']), "reddit_parse\\resources\\symbola.ttf", 36, 2060)
+
+        tfont = ImageFont.truetype("reddit_parse\\resources\\symbola.ttf", 108, encoding='unic')
+        draw.text((titlepos, 520), post['title'], font=tfont, fill=(0,0,0,255))
+
+        afont = ImageFont.truetype("reddit_parse\\resources\\symbola.ttf", 48, encoding='unic')
+        draw.text((authpos, 670), "by /u/" + post['author'], font=afont, fill=(0,0,0,255))
+
+        sfont = ImageFont.truetype("reddit_parse\\resources\\symbola.ttf", 36, encoding='unic')
+        if post['score'] > 0:
+            draw.text((scorepos, 832), "karma: " + str(post['score']), font=sfont, fill=(255,139,96,255))
+        else:
+            draw.text((scorepos, 832), "karma: " + str(post['score']), font=sfont, fill=(148,148,255,255))
+
+        log("Post %s at %s by /u/%s\n" % (post['permalink'], post['created_utc'], post['author']), "logs\\record.txt")
+        base_new.save("out\\%s" % name)
+
+def subreddit_worker(sub, stime, etime, timg):
+    """Given a subreddit and a start time,
+    parses every post through post_worker.
+    Argument sub: The subreddit name, as a string.
+    Argument stime: The time to start in epoch time, as a string.
+    Argument etime: The time to end in epoch time, as a string
+    Argument timg: See ibase in post_worker"""
+    times = numpy.arange(stime, etime, 3600) # implement your gnown gnarange function CHUM
+    try:
+        count = 0
+        for i in range(0,len(times)):
+            data = pushshift_get(sub,times[i],times[i+1])
+            for post in data:
+                post_worker(post, timg, "parsed_%s.png" % str(count).zfill(6))
+                count += 1
+    except IndexError:
+        data = pushshift_get(sub, times[i], int(time.time()))
+        for post in data:
+            post_worker(post, timg, "parsed_%s.png" % str(count).zfill(6))
 
 
 
 
+
+
+if __name__ == '__main__':
+    template = [Image.open("reddit_parse\\resources\\template.png"),(35,258,1595,1080)]
+    subreddit_worker('me_irl', 1514764800, 1514779800, template)
